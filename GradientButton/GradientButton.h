691cd43e260c3137fcdb932c300fdbda67df6d70
Royden Rego <roydenrego@softrixz.com>
2018-07-28 09:40:21 -0700

    Initial Commit

---
+++ GradientButton/GradientButton.h
@@ -0,0 +1,19 @@
+//
+//  GradientButton.h
+//  GradientButton
+//
+//  Created by ***REMOVED*** on 7/28/18.
+//  Copyright © 2018 Royden Rego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GradientButton.
+FOUNDATION_EXPORT double GradientButtonVersionNumber;
+
+//! Project version string for GradientButton.
+FOUNDATION_EXPORT const unsigned char GradientButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GradientButton/PublicHeader.h>
+
+
