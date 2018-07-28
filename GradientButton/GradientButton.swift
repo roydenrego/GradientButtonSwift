df65e79871a477d53807903cba04dd38fb2385e1
Royden Rego <roydenrego@softrixz.com>
2018-07-28 09:50:12 -0700

    Added source file

---
+++ GradientButton/GradientButton.swift
@@ -0,0 +1,48 @@
+//
+//  GradientButton.swift
+//  ***REMOVED***
+//
+//  Created by ***REMOVED*** on 7/26/18.
+//  Copyright © 2018 ***REMOVED***. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientButton: UIButton {
+    let gradientLayer = CAGradientLayer()
+    
+    @IBInspectable
+    var topGradientColor: UIColor? {
+        didSet {
+            setGradient(topGradientColor: topGradientColor, bottomGradientColor: bottomGradientColor)
+        }
+    }
+    
+    @IBInspectable
+    var bottomGradientColor: UIColor? {
+        didSet {
+            setGradient(topGradientColor: topGradientColor, bottomGradientColor: bottomGradientColor)
+        }
+    }
+    
+    private func setGradient(topGradientColor: UIColor?, bottomGradientColor: UIColor?) {
+        if let topGradientColor = topGradientColor, let bottomGradientColor = bottomGradientColor {
+            gradientLayer.frame = bounds
+            gradientLayer.colors = [topGradientColor.cgColor, bottomGradientColor.cgColor]
+            gradientLayer.borderColor = layer.borderColor
+            gradientLayer.borderWidth = layer.borderWidth
+            gradientLayer.cornerRadius = layer.cornerRadius
+            layer.insertSublayer(gradientLayer, at: 0)
+        } else {
+            gradientLayer.removeFromSuperlayer()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+            self.clipsToBounds = true
+        }
+    }
+}
